f641b42ceaaf04a946df27cdc55bc24450c4e525
Ananya Mahato <ananyaiit123@gmail.com>
2021-03-22 10:52:59 +0530

    body parser removed

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = 5000;
 
